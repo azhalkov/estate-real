8338a3c74b40846c2e60f31e52e35bd05d9772b7
azhalkov <azhalkov@yandex.ru>
2021-02-07 12:28:27 +0300

    add carousel in home

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,14 +1,33 @@
 # home/models
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.documents.models import Document
 from wagtail.documents.edit_handlers import DocumentChooserPanel
 
 from streams import blocks
+
+class HomePageCarouselImages(Orderable):
+    """ 5 фото карусели домашней страницы"""
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Для загрузки изображений',
+    )
+
+    panels = [
+        ImageChooserPanel("carousel_image"),
+    ]
+
 
 
 class HomePage(Page):
@@ -53,13 +72,21 @@ class HomePage(Page):
 
     """Поля для админки (content_panels)"""
     content_panels = Page.content_panels + [
-        FieldPanel('banner_title'),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
-        DocumentChooserPanel('book_file'),
+        MultiFieldPanel([
+            FieldPanel('banner_title'),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image"),
+            PageChooserPanel("banner_cta"),
+            DocumentChooserPanel('book_file'),
+        ], heading="Опции баннера"),
+
+        MultiFieldPanel([
+            InlinePanel("carousel_images"),
+        ], heading='Карусель фото'),
         StreamFieldPanel('content'),
     ]
+
+
 
     # panels = pass
 
